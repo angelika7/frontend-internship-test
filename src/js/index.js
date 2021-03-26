98054be540ac4e45326e5c4e0610850b50fde516
Angelika <angelika.chochorowska7@gmail.com>
2021-03-26 14:54:11 +0100

    feat: task-one, add form and active button

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-/* Here goes your JS code */
+const showPopup = document.getElementById('show-popup-form');
+const hidePopup = document.getElementById('hide-popup-form');
+const submitBtn = document.getElementById('submit-form');
+const popup = document.querySelector(".popup");
+
+// Popup appearing
+showPopup.addEventListener('click', () => {
+  popup.classList.add('show-form');
+  popup.classList.remove('hide-form');
+});
+
+// Popup hiding
+hidePopup.addEventListener('click', () => {
+  popup.classList.remove('show-form');
+  popup.classList.add('hide-form');
+});
