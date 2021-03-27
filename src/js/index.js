70e41fdca9c21a1f08962f9552486a463912cfe8
Angelika <angelika.chochorowska7@gmail.com>
2021-03-27 13:38:34 +0100

    fix: task-three, add success message

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,29 @@
+const checkbox = document.getElementById('checkbox');
+const email = document.getElementById('email');
 const form = document.querySelector('.form');
 const hidePopup = document.getElementById('hide-popup-form');
+const message = document.getElementById('message');
+const password = document.getElementById('password');
+const popup = document.querySelector('.popup');
 const showPopup = document.getElementById('show-popup-form');
 const submitBtn = document.getElementById('submit-form');
-const popup = document.querySelector('.popup');
 
 // Popup appearing
 showPopup.addEventListener('click', () => {
-  popup.classList.add('show-form');
-  popup.classList.remove('hide-form');
+  popup.classList.add('show-popup');
+  popup.classList.remove('hide-popup');
 });
 
 // Popup hiding
 hidePopup.addEventListener('click', () => {
-  popup.classList.remove('show-form');
-  popup.classList.add('hide-form');
+  popup.classList.remove('show-popup');
+  popup.classList.add('hide-popup');
 });
 
 // Validate form
 const validate = (e) => {
   e.preventDefault();
-  const password = document.getElementById('password');
-  const email = document.getElementById('email');
-  const checkbox = document.getElementById('checkbox');
-
+  
   if (email.value === "") {
     alert("Wprowadź adres email");
     email.focus();
@@ -56,10 +57,11 @@ const validate = (e) => {
   email.value = '';
   password.value = '';
   checkbox.checked = false;
-
+  
   setTimeout(() => {
-    popup.classList.add('hide-form');
-    showPopup.textContent = "Thank you!"
+    popup.classList.add('hide-popup');
+    showPopup.style.display = "none"
+    message.style.display = "block";
   }, 3000)
 
   return true; // Can submit the form data to the server
